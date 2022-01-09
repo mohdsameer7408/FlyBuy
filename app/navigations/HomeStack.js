07a24e9417d653fc3ba1e108e89183273b83d5b8
Mohd Sameer Ahmad <mohdsameer7408@gmail.com>
2022-01-09 21:58:04 +0530

    added CartScreen to HomeStack

--- app/navigations/HomeStack.js
+++ app/navigations/HomeStack.js
@@ -3,6 +3,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 
 import HomeScreen from "../screens/HomeScreen";
 import ProfileScreen from "../screens/ProfileScreen";
+import CartScreen from "../screens/CartScreen";
 
 const Stack = createStackNavigator();
 
@@ -11,6 +12,7 @@ const HomeStack = () => {
     <Stack.Navigator initialRouteName="HomeScreen">
       <Stack.Screen name="HomeScreen" component={HomeScreen} />
       <Stack.Screen name="ProfileScreen" component={ProfileScreen} />
+      <Stack.Screen name="CartScreen" component={CartScreen} />
     </Stack.Navigator>
   );
 };
